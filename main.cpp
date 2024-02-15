d5c0bd8d2008cc6bc8eb9660f82427de06a903d1
programmerKFH <kfh.freelancer@gmail.com>
2024-02-15 19:59:19 -0400

    mensaje descriptivo del commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hola mundo!" << endl;
+
+  // Esperar a que el usuario presione una tecla
+  cout << "Presione cualquier tecla para continuar..." << endl;
+  cin.get();
+
+  return 0;
+}
